61b5aa2b8aec87e21d7022e5616eb6f5d76a5bdd
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-09-06 20:22:10 -0400

    stripe payment updated

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const PORT = process.env.PORT || 8080;
+dotenv.config()
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+const configureRoutes = require("./routes");
+configureRoutes(app);
+
+app.listen(PORT, error => {
+    if (error) throw error;
+    console.log("Server running on port " + PORT);
+});
---
+++ stripe-client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import StripeBtn from "./components/StripeBtn";
+
+
+function App() {
+  return (
+    <div className="App">
+        <header className="App-header">
+          <p>Stripe Checkout - ReactJS</p>
+          <StripeBtn />
+        </header>
+      </div>
+  );
+}
+
+export default App;
