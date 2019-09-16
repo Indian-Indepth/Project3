093503b07796a30188df918a7319c5151186bab3
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2019-09-16 11:40:08 -0400

    attempted deployment

---
+++ client/build/precache-manifest.682a5eac7f25cc09caa8c4fef3cdfcbf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac600f57b6e230974c3958ffdc9eb4e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8aa77b7088fb4216fc0e",
+    "url": "/static/css/2.2316bcd1.chunk.css"
+  },
+  {
+    "revision": "0de1027fc8db6be0f2bb",
+    "url": "/static/css/main.45e218b0.chunk.css"
+  },
+  {
+    "revision": "8aa77b7088fb4216fc0e",
+    "url": "/static/js/2.a8a325c3.chunk.js"
+  },
+  {
+    "revision": "0de1027fc8db6be0f2bb",
+    "url": "/static/js/main.1e57bf00.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  },
+  {
+    "revision": "baff1fe8d4199ddf4ed9ad031767914b",
+    "url": "/static/media/bg-image.baff1fe8.png"
+  },
+  {
+    "revision": "f89fe0db42cab8b95626d75b52696ff8",
+    "url": "/static/media/logo1.f89fe0db.png"
+  }
+]);
