6c17221b3aa17d8eedbd0690f084f3556186916d
Ravi Joshi <ravipiyushjoshi@gmail.com>
2019-09-17 01:25:23 -0400

    updation with twitter

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -38,7 +38,7 @@ router.get("/google", passport.authenticate("google",{scope:['profile']}));
 
 // redirect to home page after successfully login via twitter
 router.get(
-  "auth/twitter/callback",
+  "/twitter/callback",
   passport.authenticate("twitter", {
     successRedirect: CLIENT_HOME_PAGE_URL,
     failureRedirect: "login/failed"
